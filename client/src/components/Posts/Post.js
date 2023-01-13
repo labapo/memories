30a3487d1ee7ce75e81425b14bf5f283ab6f2276
Lorivie Abapo <lorivieabapo@Lorivies-MacBook-Pro.local>
2023-01-13 12:51:12 -0700

    added UI to single post

--- client/src/components/Posts/Post.js
+++ client/src/components/Posts/Post.js
@@ -2,17 +2,26 @@ import React from "react";
 import { useSelector } from 'react-redux';
 import Post from './Post/Post';
 import useStyles from './style'
+//import grid to use
+import { Grid, CircularProgress } from '@material-ui/core'; 
 
 const Posts = () => {
     const posts = useSelector((state) => state.posts)
     const classes = useStyles();  
     console.log(posts);
     return (
-        <>
-        <h1>POSTS</h1>
-        <Post />
-        <Post />
-        </>
+        //if there are no posts, then show the spinning wheel, or else show the grid
+        !posts.length ? <CircularProgress /> : (
+            <Grid className={classes.container} container alignItems="stretch" spacing={3}>
+                {/* here we're going to map through all the posts, put each post in a grid, and render each post component.  */}
+                {/* Each component will have a key of the post id  */}
+                {posts.map((post)=> (
+                    <Grid key={post._id} item xs={12} small={6}>
+                        <Post post={post} />
+                    </Grid>
+                ))}
+            </Grid>
+        )
     )
 }
 

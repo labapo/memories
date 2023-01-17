3977149d420a1c267702c7619ab6526ca5ff5637
Lorivie Abapo <lorivieabapo@Lorivies-MacBook-Pro.local>
2023-01-17 11:07:21 -0700

    added ability to edit

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,9 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { TextField, Button, Typography, Paper } from '@material-ui/core';
 import useStyles from './style';
 import FileBase from 'react-file-base64';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { createPost, updatePost } from "../../actions/posts";
+
 
 //get the current id. We'll need to click on the 3 dots at the top of the post to edit. That'll be the edit button
 //then we'll need to pass the id in order to edit the post to the form component
@@ -11,7 +12,7 @@ import { createPost, updatePost } from "../../actions/posts";
 
 
 
-const Form = ( {currentId, setCurrentId} ) => {
+const Form = ( {currentId, setCurrentId} ) => { 
     const [postData, setPostData] = useState({
         creator: '',
         title: "",
@@ -20,8 +21,19 @@ const Form = ( {currentId, setCurrentId} ) => {
         selectedFile: ""
 
     })
+
+    //we only want the data from the updated posts. 
+    //in the function we'll do a ternary: If we have a current id, then we want to loop over state.posts and call a find method
+    //we want to find the post (p in this case) that has the same id as the current id. If they match the in puts will be put on the form
+    //if not, then it's null
+    const post = useSelector((state) => currentId ? state.posts.find((p) => p._id === currentId): null);
     const classes = useStyles(); 
     const dispatch = useDispatch(); 
+    
+    useEffect (()=> {
+        if(post) setPostData(post);
+    }, [post])
+    
     const handleSubmit = (e) => {
         //not to get the refresh in the browser
         e.preventDefault();
